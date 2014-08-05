94df5d62cc73257ad955bdd4c2a9fdd0bf79ae2a
shaosh <shaosh0913@gmail.com>
2014-08-05 15:24:16 -0700

    Update the socket.io server

--- socket.io-server/app.js
+++ socket.io-server/app.js
@@ -11,18 +11,58 @@ var socket = require('./scripts/socket.js');
 
 // io.sockets.on('connection', socket);
 //The code below are added for testing socket.io of prodApp
-var job = "{\"id\": 554133,\"name\": \"vidhyachrometest\", \"facility_id\": 2,\"location\": [], \"staff\": [],\"log\": []}";
+var job = "{\"id\": 554120,\"name\": \"vidhyachrometest\", \"facility_id\": 2,\"location\": [], \"staff\": [],\"log\": []}";
 var dataJob = JSON.parse(job);
 var dataMessage = JSON.parse("{\"job_id\": 554114, \"message\": \"This is a message for staff related to job 554114\"}");
-var dataChange = JSON.parse("{\"job\": " + job + ", \"staff_id\": 2, \"add\": false}");
+var dataChange = JSON.parse("{\"job\": " + job + ", \"staff_id\": 0, \"add\": false}");
+var user = JSON.parse("{\"id\": 0,\"name\": \"Tim\", \"facility_id\": 0,\"role_id\": 0}");
+var log = JSON.parse("{\"id\": 0,\"job_id\": \"554114\", \"job_status_id\": 5}");
+var log2 = JSON.parse("{\"id\": 0,\"job_id\": \"554114\", \"job_status_id\": 6}");
+var printlog = JSON.parse("{\"id\": 0,\"job_id\": \"554114\", \"location_id\": 0, \"print_status_id\": 4}");
+var printlog2 = JSON.parse("{\"id\": 0,\"job_id\": \"554114\", \"location_id\": 10, \"print_status_id\": 8}");
+
+var viewers = [];
 
 
 io.sockets.on('connection', function(socket){
+	console.log("Connection " + socket.id + " accepted.");
 	console.log('connection', socket);
 	socket.emit('job:received', dataJob);
+	// console.log(222);
 	// socket.emit('job:broadcast', dataMessage);
+	// socket.emit('job:changed', dataJob);
+	// socket.emit('staff:authenticated', user);
 	// socket.emit('job:staff:changed', dataChange);
+	// socket.emit('job:log:changed', log);
+	// socket.emit('job:log:changed', log2);
+	// socket.emit('job:location:changed', printlog);
+	// console.log(111);
+	// socket.emit('job:location:changed', printlog2);
+	socket.on('disconnect', function(){
+		console.log("Connection " + socket.id + " terminated.");
+	});
+	socket.on('client:job:opened', function(data){
+		console.log("Entered:\n" + JSON.stringify(data));
+		viewers.push(data.user);
+		socket.broadcast.emit("client:job:opened", viewers);
+		// fn(viewers);
+	});
+	socket.on('client:job:closed', function(data){
+		console.log("Left:\n" + JSON.stringify(data));
+		var index = -1;
+		for(var i = 0; i < viewers.length; i++){
+			if(viewers[i].id == data.user.id){
+				index = i;
+				break;
+			}
+		}
+		if(index > -1)
+			viewers.splice(index, 1);
+		socket.broadcast.emit("client:job:closed", viewers);
+	});
 });
+
+
 
 // Start server 外部访问 http://127.0.0.1:3000
 server.listen(3000, function() {
